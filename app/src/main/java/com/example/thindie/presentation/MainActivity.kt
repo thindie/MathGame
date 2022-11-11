910dec68bba2117d46230568e4cb23217d1bbc20
Alex <thindie1@mail.ru>
2022-11-11 16:11:44 +0200

    05 setting xmlns. vector icons

--- app/src/main/java/com/example/thindie/presentation/MainActivity.kt
+++ app/src/main/java/com/example/thindie/presentation/MainActivity.kt
@@ -2,9 +2,24 @@ package com.example.thindie.presentation
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
+import com.example.thindie.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        val welcomeFragment = WelcomeFragment.welcomeInstance()
+        fragmentBehavior(welcomeFragment)
+
+
+
+    }
+
+    private fun fragmentBehavior(fragment : Fragment){
+        supportFragmentManager.beginTransaction()
+            .addToBackStack(fragment.tag)
+            .attach(fragment)
+            .commit()
     }
 }
---
+++ app/src/main/java/com/example/thindie/presentation/WelcomeFragment.kt
@@ -0,0 +1,33 @@
+package com.example.thindie.presentation
+
+import android.content.Context
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.example.thindie.R
+
+class WelcomeFragment : Fragment() {
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_welcome,container,false)
+    }
+
+    override fun onAttach(context: Context) {
+        super.onAttach(context)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+
+    companion object{
+        fun welcomeInstance() : WelcomeFragment{
+            return WelcomeFragment()
+        }
+    }
+}
